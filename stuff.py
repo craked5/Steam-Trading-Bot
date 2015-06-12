8b7893edffa5e86418d0542982327307c81ef8dd
Nuno Silva <craked5@gmail.com>
2015-06-12 05:05:04 +0100

    recent parsing done!

---
+++ stuff.py
@@ -0,0 +1,35 @@
+__author__ = 'nunosilva'
+
+from smb_json_recent import SteamJsonRecent
+from smb_requests_recent import SteamBotHttp
+import time
+
+i = 0
+http = SteamBotHttp(1)
+js = SteamJsonRecent()
+times = []
+
+def median(lst):
+    lst = sorted(lst)
+    if len(lst) < 1:
+            return None
+    if len(lst) %2 == 1:
+            return lst[((len(lst)+1)/2)-1]
+    else:
+            return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
+
+while i is not 200:
+    start = time.clock()
+    recent = {}
+    recent = http.urlQueryRecent()
+    js.getRecentTotalReady(recent)
+    js.getfinallist()
+    i += 1
+    print i
+    time.sleep(0.5)
+    elapsed = time.clock()
+    elapsed = elapsed - start
+    print elapsed
+    times.append(elapsed)
+
+print median(times)
