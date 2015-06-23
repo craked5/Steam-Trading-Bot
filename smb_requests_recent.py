cc5886d21b5f1fa9545ec144f0e86655b33f6707
Nuno Silva <craked5@gmail.com>
2015-06-23 10:04:07 +0100

    alpha v1 LOGIN FUCKING WORKING FUCK YA

--- smb_requests_recent.py
+++ smb_requests_recent.py
@@ -7,6 +7,10 @@ __author__ = 'github.com/craked5'
 import requests as req
 import ast
 import ujson
+import time
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
 
 class SteamBotHttp:
 
@@ -51,7 +55,23 @@ class SteamBotHttp:
             'Referer':'http://steamcommunity.com/market/',
             'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'
         }
-
+        self.rsa_headers = {
+            'Accept':'text/javascript, text/html, application/xml, text/xml, */*',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'pt-PT,pt;q=0.8,en-US;q=0.6,en;q=0.4,fr;q=0.2,es;q=0.2',
+            'Connection':'keep-alive',
+            'Content-Length':44,
+            'Content-type':'application/x-www-form-urlencoded; charset=UTF-8',
+            'Cookie':'steamMachineAuth76561197979199766=5682D02C36EBD479EC086107B2EC135E267C9385; __utma=268881843.1944006538.1426348260.1426845397.1427022271.24; __utmz=268881843.1427022271.24.22.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); Steam_Language=english; 730_17workshopQueueTime=1432014476; recentlyVisitedAppHubs=220%2C316950%2C440%2C72850%2C295110%2C730; sessionid=5cfbd35e404358ce92d5aaa0; steamCountry=PT%7C90d987902b02ceec924245352748dc71; strInventoryLastContext=730_2; timezoneOffset=3600,0',
+            'CSP':'active',
+            'DNT':1,
+            'Host':'steamcommunity.com',
+            'Origin':'https://steamcommunity.com',
+            'Referer':'https://steamcommunity.com/login/home/?goto=0',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+            'X-Prototype-Version':1.7,
+            'X-Requested-With':'XMLHttpRequest'
+        }
         #price = preco que eu quero receber = price+fee_price
         self.data_sell = {
             "sessionid" : self.sessionid,
@@ -72,24 +92,46 @@ class SteamBotHttp:
             'total': 0,
             'quantity': 1
         }
-        
-        self.cookies2 = {
-            'steamMachineAuth76561197979199766':'5682D02C36EBD479EC086107B2EC135E267C9385',
-            '__utma':'268881843.1944006538.1426348260.1426845397.1427022271.24',
-            '__utmz':'268881843.1427022271.24.22.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
-            'Steam_Language':'english',
-            '730_17workshopQueueTime':'1432014476',
-            'steamRememberLogin':'76561197979199766%7C%7Cdf433a77e3eee7d7e472716c8ce2dfba',
-            'recentlyVisitedAppHubs':'220%2C316950%2C440%2C72850%2C295110%2C730',
-            'sessionid':'9d8e0a5043cccddd6c430395',
-            'webTradeEligibility':'%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22sales_this_year%22%3A101%2C%22max_sales_per_year%22%3A200%2C%22forms_requested%22%3A0%2C%22new_device_cooldown_days%22%3A7%7D',
-            'steamCountry':'PT%7C90d987902b02ceec924245352748dc71',
-            'steamLogin':'76561197979199766%7C%7C9E4F945373E086AE0ABD1A71CEEC718241E2E2B2',
-            'steamLoginSecure':'76561197979199766%7C%7CEEF7B52C4A0259FBA5D09A596F0CE2484EAE7170',
-            'strInventoryLastContext':'730_2',
-            'tsTradeOffersLastRead':'1434610877',
-            'timezoneOffset':'3600,0'
+        self.login_data = {
+            'password':self.encrypted_password,
+            'username':'freeman777',
+            'twofactorcode':'',
+            'emailauth':'',
+            'loginfriendlyname':'',
+            'captchagid':-1,
+            'captcha_text':'',
+            'emailsteamid':'',
+            'rsatimestamp': '',
+            'remember_login':'true',
+            'donotcache': self.donotcache
         }
+        self.password = 'Steamgresso1234567@'
+        self.encrypted_password = ''
+        self.donotcache = 0
+        self.rsa_data = {
+            'username': 'freeman777',
+            'donotcache': self.donotcache
+        }
+
+    def login(self):
+        self.donotcache = self.now_milliseconds()
+        temp_rsa = req.post('https://steamcommunity.com/login/getrsakey/', headers=self.rsa_headers, data=self.rsa_data)
+        print temp_rsa.content
+        temp_ras_good = ujson.loads(temp_rsa.content)
+        self.login_data['rsatimestamp'] = temp_ras_good['rsatimestamp']
+        mod = long(temp_ras_good['publickey_mod'], 16)
+        exp = long(temp_ras_good['publickey_exp'], 16)
+        rsa_key = RSA.construct((mod, exp))
+        rsa = PKCS1_v1_5.PKCS115_Cipher(rsa_key)
+        encrypted_password = rsa.encrypt(self.password)
+        encrypted_password = base64.b64encode(encrypted_password)
+        self.encrypted_password = encrypted_password
+        temp_dologin = req.post('https://steamcommunity.com/login/dologin/', headers=self.rsa_headers, data=self.login_data)
+        print temp_dologin.content
+        print temp_dologin.status_code
+
+    def now_milliseconds(self):
+        self.donotcache = int(time.time() * 1000)
 
     def urlQueryItem(self,item):
         steam_response = req.get(self.complete_url_item + item)
--- test_login.py
+++ test_login.py
@@ -10,15 +10,6 @@ import time, datetime
 def now_milliseconds():
    return int(time.time() * 1000)
 
-# reference time.time
-# Return the current time in seconds since the Epoch.
-# Fractions of a second may be present if the system clock provides them.
-# Note: if your system clock provides fractions of a second you can end up
-# with results like: 1405821684785.2
-# our conversion to an int prevents this
-
-def date_time_milliseconds(date_time_obj):
-   return int(time.mktime(date_time_obj.timetuple()) * 1000)
 
 # reference: time.mktime() will
 # Convert a time tuple in local time to seconds since the Epoch.
