05da3b151b427a00da249766e5068635bd38b240
Nuno Silva <craked5@gmail.com>
2015-06-24 04:34:00 +0100

    alpha v1 LOGIN FUCKING WORKING FUCK YA

--- test_login.py
+++ test_login.py
@@ -37,6 +37,21 @@ rsa_headers = {
     'X-Prototype-Version':1.7,
     'X-Requested-With':'XMLHttpRequest'
 }
+transfer_headers = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding':'gzip, deflate',
+    'Accept-Language':'pt-PT,pt;q=0.8,en-US;q=0.6,en;q=0.4,fr;q=0.2,es;q=0.2',
+    'Cache-Control':'max-age=0',
+    'Connection':'keep-alive',
+    'Content-Length':184,
+    'Content-Type':'application/x-www-form-urlencoded',
+    'Cookie':'browserid=740048883567382728; steamMachineAuth76561197979199766=5682D02C36EBD479EC086107B2EC135E267C9385; __utma=128748750.2096343856.1422402525.1426615286.1426972563.6; __utmz=128748750.1426972563.6.5.utmcsr=etterstudio.com|utmccn=(referral)|utmcmd=referral|utmcct=/en/pnp.php; dp_user_userid=10011565; dp_user_password=aEjiUkC3; lastagecheckage=1-January-1981; recentapps=%7B%22362890%22%3A1430846718%2C%22295110%22%3A1430761022%2C%22252490%22%3A1430173804%2C%22271590%22%3A1428971636%2C%22290930%22%3A1427354378%2C%22353560%22%3A1426972561%2C%22262390%22%3A1424562332%2C%22239140%22%3A1422402521%7D; timezoneOffset=3600,0; steamCountry=PT%7Ceadce223f9093afc9f086e613abc8402; steamRememberLogin=76561197979199766%7C%7Cdf433a77e3eee7d7e472716c8ce2dfba; dp_user_sessionid=8b4e6fb7c1b244f4330561fa7d87ca7d; steamLogin=76561197979199766%7C%7C9E4F945373E086AE0ABD1A71CEEC718241E2E2B2; steamLoginSecure=76561197979199766%7C%7CEEF7B52C4A0259FBA5D09A596F0CE2484EAE7170',
+    'DNT':1,
+    'Host':'steamcommunity.com',
+    'Origin':'https://steamcommunity.com',
+    'Referer':'https://steamcommunity.com/login/home/?goto=0',
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+}
 
 
 temp_rsa = requests.post('https://steamcommunity.com/login/getrsakey/', headers=rsa_headers, data=rsa_data)
@@ -65,9 +80,33 @@ login_data = {
     'donotcache': donot
 }
 
+
+
 print encrypted_password
 
 temp_dologin = requests.post('https://steamcommunity.com/login/dologin/', headers=rsa_headers, data=login_data)
 
 print temp_dologin.content
 print temp_dologin.status_code
+
+temp_dologin_good = ujson.loads(temp_dologin.content)
+print temp_dologin_good
+
+time.sleep(2)
+transfer_data = {
+    'steamid': 0,
+    'token':'',
+    'auth':'',
+    'remember_login':'',
+    'token_secure':''
+}
+
+transfer_data['steamid'] = temp_dologin_good['transfer_parameters']['steamid']
+transfer_data['token'] = temp_dologin_good['transfer_parameters']['token']
+transfer_data['auth'] = temp_dologin_good['transfer_parameters']['auth']
+transfer_data['remember_login'] = temp_dologin_good['transfer_parameters']['remember_login']
+transfer_data['token_secure'] = temp_dologin_good['transfer_parameters']['token_secure']
+
+temp_transfer = requests.post('https://store.steampowered.com/login/transfer', headers=transfer_headers,data=transfer_data)
+print temp_transfer.content
+print temp_transfer.status_code
